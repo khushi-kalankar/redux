d6dd6c0d93ee9c70d75294246eacabbb8f546342
Khushi <khushikalankar@gmail.com>
2024-12-15 01:36:44 +0530

    added slice

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import counterSlice  from "./slices/counter";
 
 export const store = configureStore({
-    reducer: {}, //reducer handles events
+    reducer: {
+        counter: counterSlice,
+    }, //reducer handles events
 })
 
 export type RootState = ReturnType<typeof store.getState>
